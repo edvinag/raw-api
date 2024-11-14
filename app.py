1ef7400f6db219eddac10969d187f1288348e243
edvinag <edvin.agnas@gmail.com>
2024-11-14 13:19:21 +0100

    Add env

--- app.py
+++ app.py
@@ -3,27 +3,26 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# Dictionary to store the location data
-location_data = {}
+# Load the initial IP address from the environment variable, if set
+ip_data = {"ip_address": os.environ.get("IP_ADDRESS", None)}
 
-@app.route('/set_location', methods=['POST'])
-def set_location():
+@app.route('/set_ip', methods=['POST'])
+def set_ip():
     data = request.get_json()
-    if 'location' in data:
-        location_data['location'] = data['location']
-        return jsonify({"message": "Location set successfully!"}), 200
+    if 'ip_address' in data:
+        # Update the IP address in the environment variable
+        ip_data['ip_address'] = data['ip_address']
+        return jsonify({"message": "IP address set successfully!"}), 200
     else:
-        return jsonify({"error": "No location provided"}), 400
+        return jsonify({"error": "No IP address provided"}), 400
 
-@app.route('/get_location', methods=['GET'])
-def get_location():
-    if 'location' in location_data:
-        return jsonify({"location": location_data['location']}), 200
+@app.route('/get_ip', methods=['GET'])
+def get_ip():
+    if ip_data.get('ip_address'):
+        return jsonify({"ip_address": ip_data['ip_address']}), 200
     else:
-        return jsonify({"error": "No location set"}), 404
+        return jsonify({"error": "No IP address set"}), 404
 
 if __name__ == '__main__':
-    # Get the port from the environment (Render provides this automatically)
     port = int(os.environ.get("PORT", 5000))
-    # Run the app, binding to 0.0.0.0 to be accessible externally
     app.run(host='0.0.0.0', port=port)
